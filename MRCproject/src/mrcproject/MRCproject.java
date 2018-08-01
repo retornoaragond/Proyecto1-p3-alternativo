be199816aaa64d5194162b9ef715a04847dcb47a
CarlosAndrés <CarlosAndrs@Carlos>
2018-08-01 01:08:28 -0600

    prueba

--- MRCproject/src/mrcproject/MRCproject.java
+++ MRCproject/src/mrcproject/MRCproject.java
@@ -15,7 +15,10 @@ public class MRCproject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    
+        System.out.println("holaaa");
+
+// TODO code application logic here
     }
     
 }
