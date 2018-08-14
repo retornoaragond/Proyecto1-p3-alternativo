692e830c790652190c2fc77dd5f67d12e6c3ad35
retornoaragond <eef251195@gmail.com>
2018-08-14 13:36:15 -0600

    ultimos retoques para el entregable

--- MRCproject/src/mrcproject/MRCproject.java
+++ MRCproject/src/mrcproject/MRCproject.java
@@ -10,8 +10,12 @@ import mrcproject.model.Proyecto;
 public class MRCproject {
 
     public static void main(String[] args) throws Exception {
-        Proyecto prueba = new Proyecto(new Archivos().carga("datos.xml"));//carga el archivo
-        System.out.print("\n"+prueba.rutaCritica()+"\n\n");// se imprime lo cargado
-        System.out.print(prueba.Prueba_inicial());// se imprime lo cargado
+        try {
+            Proyecto prueba = new Proyecto(new Archivos().carga("datos3s.xml"));//carga el archivo
+            System.out.print("\n" + prueba.rutaCritica() + "\n\n");// se imprime lo cargado
+            System.out.print(prueba.Prueba_inicial());// se imprime lo cargado
+        } catch (Exception e) {
+            System.err.println("El programa no puede ejecutarse no se ha encontrado el archivo");
+        }
     }
 }
--- MRCproject/src/mrcproject/model/Archivos.java
+++ MRCproject/src/mrcproject/model/Archivos.java
@@ -2,52 +2,62 @@ package mrcproject.model;
 
 import java.util.HashMap;
 import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 
 /**
  * @author Esteban Espinoza Fallas
  * @author Carlos Vargas Alfaro
  */
-
 public class Archivos {
 
     public Archivos() {
     }
 
-    public HashMap<String, Actividad> carga(String path) throws Exception {
-        DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
-        Document doc = docBuilder.parse(new File(path));
-        doc.getDocumentElement().normalize();
-        HashMap actividades = new HashMap();
-        Actividad temp;
+    public HashMap<String, Actividad> carga(String path) {
+        try {
+            DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+            Document doc = null;
+            HashMap actividades = new HashMap();
+                doc = docBuilder.parse(new File(path));
+                Actividad temp;
+                NodeList as = doc.getElementsByTagName("Actividad");
+                for (int i = 0; i < as.getLength(); i++) {
+                    Node n = as.item(i);
+                    if (n.getNodeType() == Node.ELEMENT_NODE) {
+                        Element a = (Element) n;
+                        actividades.put(a.getAttribute("id"), new Actividad(a.getAttribute("id"),
+                                Integer.parseInt(a.getAttribute("duracion"))));
+                    }
+                }
 
-        NodeList as = doc.getElementsByTagName("Actividad");
-        for (int i = 0; i < as.getLength(); i++) {
-            Node n = as.item(i);
-            if (n.getNodeType() == Node.ELEMENT_NODE) {
-                Element a = (Element) n;
-                actividades.put(a.getAttribute("id"), new Actividad(a.getAttribute("id"), 
-                        Integer.parseInt(a.getAttribute("duracion"))));
-            }
+                as = doc.getElementsByTagName("Relacion");
+                for (int i = 0; i < as.getLength(); i++) {
+                    Node n = as.item(i);
+                    if (n.getNodeType() == Node.ELEMENT_NODE) {
+                        Element a = (Element) n;
+                        temp = (Actividad) actividades.get(a.getAttribute("actividad"));
+                        temp.getSalidas().add((Actividad) actividades.get(a.getAttribute("sucesor")));
+                        temp = (Actividad) actividades.get(a.getAttribute("sucesor"));
+                        temp.getEntradas().add((Actividad) actividades.get(a.getAttribute("actividad")));
+                    }
+                }
+            doc.getDocumentElement().normalize();
+            return (HashMap) actividades;
+        } catch (Exception ex) {
+            System.out.println("No se encontro el archivo");
+            System.out.println("No se pudo cargar el archivo");
         }
-
-        as = doc.getElementsByTagName("Relacion");
-        for (int i = 0; i < as.getLength(); i++) {
-            Node n = as.item(i);
-            if (n.getNodeType() == Node.ELEMENT_NODE) {
-                Element a = (Element) n;
-                temp = (Actividad)actividades.get(a.getAttribute("actividad"));
-                temp.getSalidas().add((Actividad)actividades.get(a.getAttribute("sucesor")));
-                temp = (Actividad)actividades.get(a.getAttribute("sucesor"));
-                temp.getEntradas().add((Actividad)actividades.get(a.getAttribute("actividad")));
-            }
-        }
-        return (HashMap)actividades;
+        return null;
     }
 }
